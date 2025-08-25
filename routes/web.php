999c4bb3fab999360f4c69e9aeda4f7f43acf27a
honour112 <keblehonour@gmail.com>
2025-08-25 09:31:09 +0100

    fitrst

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/home', function () {
+    return view('home-page');
+
+})->name('home-page');
+Route::get('/about', function () {
+   return view('about-page');
+})->name('about-page');
+Route::get('/menu', function () {
+    return view('menu-page');
+})->name('menu-page');
+Route::get('/review', function () {
+    return view('review-page');
+})->name('review-page');
+Route::get('/contact', function () {
+    return view('contact-page');
+})->name('contact-page');
+Route::get('/book-table', function () {
+    return view('booktable-page');
+})->name('booktable-page');
+Route::get('/admindashboard', function () {
+    return view('admin-dashboard');
+})->name('/admin-dashboard');
