ed5be46b20c97797916b4c291bd8bd2e4e047e37
honour112 <keblehonour@gmail.com>
2025-08-28 10:25:36 +0100

    Update version

--- routes/web.php
+++ routes/web.php
@@ -13,31 +13,102 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    return view('home-page');
-
+// Website Routes
+Route::get('/', function () {
+    return view('website.home-page');
 })->name('home-page');
+
 Route::get('/about', function () {
-   return view('about-page');
+    return view('website.about-page');
 })->name('about-page');
+
 Route::get('/menu', function () {
-    return view('menu-page');
+    return view('website.menu-page');
 })->name('menu-page');
+
 Route::get('/review', function () {
-    return view('review-page');
+    return view('website.review-page');
 })->name('review-page');
+
 Route::get('/contact', function () {
-    return view('contact-page');
+    return view('website.contact-page');
 })->name('contact-page');
-Route::get('/book-table', function () {
-    return view('booktable-page');
+
+Route::get('/booktable', function () {
+    return view('website.booktable-page');
 })->name('booktable-page');
-Route::get('/frontdesk-dashboard', function () {
-    return view('frontdesk-dashboard');
+
+// // Admin & Delivery Dashboards
+// Route::get('/admin-dashboard', function () {
+//     return view('admin-dashboard');
+// })->name('admin.dashboard');
+
+// Route::get('/delivery-dashboard', function () {
+//     return view('delivery-dashboard');
+// })->name('delivery.dashboard');
+
+// // Front Desk Routes
+//  Route::get('/dashboard', function () {
+//  return view('frontdesk.dashboard');
+//    })->name('dashboard');
+
+//     Route::get('/manage-orders', function () {
+//         return view('frontdesk.manageorders');
+//     })->name('manage-orders');
+
+//     Route::get('/manage-bookings', function () {
+//         return view('frontdesk.managebookings');
+//     })->name('manage-bookings');
+
+//     Route::get('/reviews', function () {
+//         return view('frontdesk.reviews');
+//     })->name('reviews');
+
+//     Route::get('/signout', function () {
+//         return view('frontdesk.signout');
+//     })->name('signout');
+
+//     Route::get('/assign-delivery', function () {
+//         return view('frontdesk.assigndelivery');
+//     })->name('assign-delivery');
+
+//     Route::get('/test', function () {
+//         return view('frontdesk.test');
+//     })->name('test');
+Route::get('/assigndelivery', function () {
+    return view('frontdesk.assigndelivery');
+})->name('assigndelivery');
+Route::get('/sidebar', function () {
+    return view('frontdesk.sidebar');
+})->name('sidebar');
+
+Route::get('/frontdesk', function () {
+    return view('frontdesk.frontdesk-dashboard');
 })->name('frontdesk-dashboard');
-Route::get('/admin-dashboard', function () {
-    return view('admin-dashboard');
-})->name('admin-dashboard');
-Route::get('/delivery-dashboard', function () {
-    return view('delivery-dashboard');
-})->name('delivery-dashboard');
+
+Route::get('/dashboard', function () {
+    return view('frontdesk.dashboard');
+})->name('dashboard');
+
+Route::get('/assigndelivery', function () {
+    return view('frontdesk.assigndelivery');
+})->name('assigndelivery');
+
+Route::get('/managebookings', function () {
+    return view('frontdesk.managebookings');
+})->name('managebookings');
+
+Route::get('/manageorders', function () {
+    return view('frontdesk.manageorders');
+})->name('manageorders');
+
+Route::get('/signout', function () {
+    return view('frontdesk.signout');
+})->name('signout');
+
+Route::get('/reviews', function () {
+    return view('frontdesk.reviews');
+})->name('reviews');
+
+
+
